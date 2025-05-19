51a3460fe8061c30ead95a909adf18dfd19bb845
Juliette Campello <campello.juliette@gmail.com>
2025-05-19 09:47:23 +0200

    add of comments

--- dvd.js
+++ dvd.js
@@ -1,39 +1,46 @@
-const objdvd = document.querySelector("#objdvd");
+const objdvd = document.querySelector("#objdvd"); // Select the DVD logo element
 
+/* Initial position of the DVD logo */
 posX = 400;
 posY = 250;
 
+/* Speed of the DVD logo */
 speedX = 0.5;
 speedY = 0.5;
 
-const dvdHeight = 150;
+/* Height and Width of the DVD logo */
+const dvdHeight = 150; // reduce the gap on the bottom present because the background of the logo is too big
 const dvdWidth = 300;
 
 function moovingDvd() {
+    /* Get the height and width of the window */
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
 
-    if (posX + dvdWidth >= windowWidth || posX <= 0) {
-        speedX *= -1;
-        changeLogoColor();
+    if (posX + dvdWidth >= windowWidth || posX <= 0) {  // Check if the logo as reach the end of the window
+        speedX *= -1; // Reverse the speed
+        changeLogoColor(); // Change the color of the logo when it hits the wall
     }
     if (posY + dvdHeight >= windowHeight || posY <= 0) {
         speedY *= -1;
         changeLogoColor();
     }
 
+    /* Update the position x and y of the logo */
     posX += speedX;
     posY += speedY;
 
+    /* Update the position of the logo */
     objdvd.style.left = posX + "px";
     objdvd.style.top = posY + "px";
 
-    requestAnimationFrame(moovingDvd);
+    requestAnimationFrame(moovingDvd); // Call the function again to create an animation
 }
 
+/* Function to change the color of the logo*/
 function changeLogoColor() {
-    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    objdvd.style.fill = randomColor;
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`; // Generate a random color
+    objdvd.style.fill = randomColor; // Change the color of the logo by changing the fill property
 }
 
-moovingDvd();
+moovingDvd(); // Call the function to start the animation
